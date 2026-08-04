a1db0f4162b257b95780a2fc07a389f4d4ffaa56
Hamza Amir <hamzaamir9733@gmail.com>
2026-08-04 10:24:52 +0500

    Create 1.chatmodel_openai.py

---
+++ Models/2.ChatModels/1.chatmodel_openai.py
@@ -0,0 +1,10 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-4", temperature=0.7, max_completion_tokens=10)
+
+result = model.invoke("Write a short story about a robot learning to love.")
+
+print("ChatModel Answer:\n", result.content)
