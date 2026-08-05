9c2df01167491bc8a09c05b6b5a94c12f78bce52
Hamza Amir <hamzaamir9733@gmail.com>
2026-08-05 18:45:29 +0500

    Create chat_prompt_template.py

---
+++ Prompts/chat_prompt_template.py
@@ -0,0 +1,13 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+chat_template = ChatPromptTemplate(
+    [
+        ("system", "You are a helpful {domain} expert."),
+        ("human", "Explain in simple terms: what is {topic}?"),
+    ]
+)
+
+# Pass variables inside a dictionary argument
+prompt = chat_template.invoke({"domain": "cricket", "topic": "Doosra"})
+
+print(prompt)
